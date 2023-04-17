c7f7b8844e1378fcc3c959bb3109bb7c1ffaefd4
Maximo Yampa <maximo.yampaet12d1@gmail.com>
2023-04-17 17:22:28 -0300

    comit 2

--- Bolillero.core/Bolillero.cs
+++ Bolillero.core/Bolillero.cs
@@ -1,29 +1,38 @@
 namespace Bolillero.core;
 public class Bolillero
 {
-    public List<int> BolillasNoJugadas { get; set; }
+    public List<int> adentro { get; set; }
     public List<int> Jugada { get; set; }
-    public List<int> BolillasJugadas { get; set; }
+    public List<int> afuera { get; set; }
 
     public IAzar Azar { get; set; }
 
     public Bolillero()
     {
-        this.BolillasNoJugadas = new List<int>();
+        this.adentro = new List<int>();
         this.Jugada = new List<int>();
-        this.BolillasJugadas = new List<int>();
+        this.afuera = new List<int>();
     }
 
     public int SacarBolillas()
     {
-        var indice = Azar.SacarIndice(this.BolillasNoJugadas);
+        var indice = Azar.SacarIndice(this.adentro);
+        var elemento = adentro[indice];
 
-        this.BolillasJugadas.Remove(indice);
-        this.BolillasNoJugadas.Add(indice);
+        this.afuera.Add(elemento);
+        this.adentro.RemoveAt(elemento);
+
+        return indice;
     }
 
     public bool Jugar(List<int> jugada)
+
     {
+        for(int i=0; i <= jugada.Count() ; i ++);
+        {
+            SacarBolillas();
+        }
+        return false;
     }
 
     public int JugarNVeces(List<int> Jugada, int cantidad)
