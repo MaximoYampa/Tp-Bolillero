d188fd277cba06677a825e3a5037899a8c24ea60
MaximoYampa <maximo.yampaet12d1@gmail.com>
2023-05-17 16:50:48 -0300

    b

--- BolilleroApp.core/Simulacion.cs
+++ BolilleroApp.core/Simulacion.cs
@@ -2,8 +2,30 @@ namespace BolilleroApp.core;
 
 public class Simulacion
 {
-    long SimulacionSinHilos(Bolillero copia, List<int> Jugada, int CantidadDeSimulaciones)
+    long SimulacionSinHilos(Bolillero copia, List<int> jugada, int cantidadDeSimulaciones)
 
-        => copia.JugarNVeces(Jugada, CantidadDeSimulaciones);
+        => copia.JugarNVeces(jugada, cantidadDeSimulaciones);
+
+
+    long SimulacionConHilos(Bolillero copia, List<int> jugada, int cantidadDeSimulaciones, int hilos)
+
+    {
+        Task<long>[] Simulaciones = new Task<long>[hilos];
+        var division = cantidadDeSimulaciones / hilos;
+
+
+        for (int i = 0; i < cantidadDeSimulaciones; i++) ;
+        {
+            var BolilleroCopia = copia.Clonar();
+
+            Simulaciones[i] = Task<long>.Run(() => (long)BolilleroCopia.JugarNVeces(Jugada, 10000));
+
+        }
+        Task<long>.WaitAll(Simulaciones);
+
+
+        return 0;
+    }
+
 
 }
